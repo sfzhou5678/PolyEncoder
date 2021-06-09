cfa69ba852af9c66305e2ddae867a3c8b8c0b352
zsf <260174969@qq.com>
2021-06-09 19:49:14 +0800

    fix a bug in dot_attention()

--- model/attention.py
+++ model/attention.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 def dot_attention(q, k, v, v_mask=None, dropout=None):
   attention_weights = torch.matmul(q, k.transpose(-1, -2))
   if v_mask is not None:
-    attention_weights *= v_mask.unsqueeze(1)
+    extended_v_mask = (1.0 - v_mask.unsqueeze(1)) * -100000.0
+    attention_weights += extended_v_mask
   attention_weights = F.softmax(attention_weights, -1)
   if dropout is not None:
     attention_weights = dropout(attention_weights)
